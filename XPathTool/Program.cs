b518bbe7905b38784eb046987f54b8d7709f2111
f14shm4n <uraborg@gmail.com>
2018-07-15 21:55:56 +0300

    Добавьте файлы проекта.

---
+++ XPathTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XPathTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
